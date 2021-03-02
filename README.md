fdb3f8b1ce62dbc532d9434ea753a212d997e2e4
Andres802 <reachandres@gmail.com>
2021-03-02 00:43:47 +0000

    instagram clone

---
+++ README.md
@@ -0,0 +1,32 @@
+# Platzigram
+
+Create a minimal project (inspired by instagram) in Django.
+
+![](https://i.imgur.com/4VotR0d.png)
+
+## Dependencies
+Python 3
+Django
+
+## Usage
+```shell
+python3 -m venv .env
+source .env/bin/activate
+pip install -r requirements.txt
+```
+
+```python
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
+```
+
+## Course
+[Platzi Django Course](https://platzi.com/cursos/django/)
+
+
+## License
+[This project is under MIT License](https://opensource.org/licenses/MIT)
+
+## Author
+[Andres Barrera](https://github.com/Andres802) | 2021
---
+++ posts/views.py
@@ -0,0 +1,45 @@
+"""Posts views."""
+
+# Django
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DetailView, ListView
+
+# Forms
+from posts.forms import PostForm
+
+# Models
+from posts.models import Post
+
+
+class PostsFeedView(LoginRequiredMixin, ListView):
+    """Return all published posts."""
+
+    template_name = 'posts/feed.html'
+    model = Post
+    ordering = ('-created',)
+    paginate_by = 30
+    context_object_name = 'posts'
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    """Return post detail."""
+
+    template_name = 'posts/detail.html'
+    queryset = Post.objects.all()
+    context_object_name = 'post'
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    """Create a new post."""
+
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
+
+    def get_context_data(self, **kwargs):
+        """Add user and profile to context."""
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
